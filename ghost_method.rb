a5e5ffbfe1808f2b4002bca236ca643df0259148
Ko Kumagai <ko.kumagai.ko@gmail.com>
2015-08-02 20:11:47 -0700

    add ghost_method.rb

--- ghost_method.rb
+++ ghost_method.rb
@@ -2,14 +2,14 @@ class Ghost
   def method_missing(name, *args, &block)
     result = "You called => #{name}(#{args.join(',')})"
     result +=  "{ #{block.call} }" if block_given?
-    result
+    puts result
   end
 end
 
 ghost = Ghost.new
-puts ghost.ghost_method
+ghost.ghost_method
 #=> You called => ghost_method()
-puts ghost.ghost_method_with_arguments('a', 1)
+ghost.ghost_method_with_arguments('a', 1)
 #=> You called => ghost_method_with_arguments(a,1)
-puts ghost.ghost_method_with_block('a', 1) { 'This is a block' }
+ghost.ghost_method_with_block('a', 1) { 'This is a block' }
 #=> You called => ghost_method_with_block(a,1){ This is a block }
