9358e282b600261b39f93868cb0331054fa4780f
Ko Kumagai <ko.kumagai.ko@gmail.com>
2015-08-06 18:13:36 -0700

    add ghost_method.rb

--- ghost_method.rb
+++ ghost_method.rb
@@ -9,7 +9,9 @@ end
 ghost = Ghost.new
 ghost.ghost_method
 #=> You called => ghost_method()
+
 ghost.ghost_method_with_arguments('a', 1)
 #=> You called => ghost_method_with_arguments(a,1)
+
 ghost.ghost_method_with_block('a', 1) { 'This is a block' }
 #=> You called => ghost_method_with_block(a,1){ This is a block }
