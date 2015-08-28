b824f902098b5893faea9e1bb6002a00e331b016
Ko Kumagai <ko.kumagai.ko@gmail.com>
2015-08-27 19:33:35 -0700

    add dynamic_method.rb

---
+++ dynamic_method.rb
@@ -0,0 +1,21 @@
+class A; end
+
+A.class_eval do
+  define_method :dynamic_method do
+    puts 'dynamic_method : A#dynamic_method'
+  end
+end
+
+A.new.dynamic_method #=>
+
+
+class B
+  def self.create_method(method)
+    define_method method do
+      yield
+    end
+  end
+end
+
+B.create_method('dynamic_method') { puts 'dynamic_method : B#dynamic_method' }
+B.new.dynamic_method
