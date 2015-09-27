0405224f9aa9bb74d77f1ab6dfed9910b0881482
Ko Kumagai <ko.kumagai.ko@gmail.com>
2015-09-26 20:04:48 -0700

    add percent_notation.rb

---
+++ percent_notation.rb
@@ -0,0 +1,57 @@
+name = 'Ko'
+
+# %, %Q
+p %(Matz DHH #{name}) #=> "Matz DHH Ko"
+p %Q(Matz DHH #{name}) #=> "Matz DHH Ko"
+# equivalent to
+p "Matz DHH #{name}" #=> "Matz DHH Ko"
+
+
+# %q
+p %q(Matz DHH #{name}) #=> "Matz DHH \#{name}"
+# equivalent to
+p 'Matz DHH #{name}' #=> "Matz DHH \#{name}"
+
+
+# %W
+p %W(Matz DHH #{name}) #=> ["Matz", "DHH", "Ko"]
+# equivalent to
+p ["Matz", "DHH", "#{name}"] #=> ["Matz", "DHH", "Ko"]
+
+
+# %w
+p %w(Matz DHH #{name}) #=> ["Matz", "DHH", "\#{name}"]
+# equivalent to
+p ['Matz', 'DHH', '#{name}'] #=> ["Matz", "DHH", "\#{name}"]
+
+
+# %I
+p %I(Matz DHH #{name}) #=> [:Matz, :DHH, :Ko]
+# equivalent to
+p [:Matz, :DHH, :"#{name}"] #=> [:Matz, :DHH, :Ko]
+
+
+# %i
+p %i(Matz DHH #{name}) #=> [:Matz, :DHH, :"\#{name}"]
+# equivalent to
+p [:Matz, :DHH, :'#{name}'] #=> [:Matz, :DHH, :"\#{name}"]
+
+
+# %x
+p %x(date)
+# equivalent to
+p `date`
+
+
+# %s
+p %s(Ko) #=> :Ko
+p %s(#{name}) #=> :"\#{name}"
+# equivalent to
+p :Ko #=> :Ko
+p :'#{name}' #=> :"\#{name}"
+
+
+# %r
+p %r(^http://www.kokumagai.com/) #=> /^http:\/\/www.kokumagai.com\//
+# equivalent to
+p /^http:\/\/www.kokumagai.com\// #=> /^http:\/\/www.kokumagai.com\//
